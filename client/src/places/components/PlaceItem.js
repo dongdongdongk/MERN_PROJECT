c27e5815340026881ff596b95b942b083187a86a
dongdongdongk <dhk9309@gmail.com>
2024-01-22 01:57:22 +0900

    Auth

--- client/src/places/components/PlaceItem.js
+++ client/src/places/components/PlaceItem.js
@@ -9,16 +9,31 @@ import './PlaceItem.css'
 const PlaceItem = (props) => {
     const [showMap, setShowMap] = useState(false);
 
+    const [showConfirmModal, setShowConfirmModal] = useState(false);
+
     const openMapHandler = () => setShowMap(true);
 
     const closeMapHandler = () => setShowMap(false);
+
+    const showDeleteWaringHandler = () => {
+        setShowConfirmModal(true)
+    };
+
+    const cancelDeleteHandler = () => {
+        setShowConfirmModal(false)
+    }
+
+    const confirmDeleteHandler = () => {
+        setShowConfirmModal(false)
+        console.log('DELETING....')
+    }
 
     return (
         <>
             <Modal
                 show={showMap}
                 onCancel={closeMapHandler}
-                header={props.address} 
+                header={props.address}
                 contentClass="place-item__modal-content"
                 footerClass="place-item__modal-actions"
                 footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
@@ -26,8 +41,24 @@ const PlaceItem = (props) => {
                 <div className="map-container">
                     <h2>THE MAP</h2>
                 </div>
-
             </Modal>
+
+            <Modal
+                show={showConfirmModal}
+                onCancel={cancelDeleteHandler}
+                header="Are you sure?"
+                footerClass="place-item__modal-actions"
+                footer={
+                    <>
+                        <Button inverse onClick={cancelDeleteHandler}>CANCEL</Button>
+                        <Button danger onClick={confirmDeleteHandler}>DELETE</Button>
+                    </>
+                }>
+                <p>
+                    정말 삭제 하시겠습니까?
+                </p>
+            </Modal>
+
             <li className="place-item">
                 <Card className="place-item__content">
                     <div className="place-item__image">
@@ -39,9 +70,15 @@ const PlaceItem = (props) => {
                         <p>{props.description}</p>
                     </div>
                     <div className="place-item__actions">
-                        <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
-                        <Button to={`/places/${props.id}`}>EDIT</Button>
-                        <Button danger>DELETE</Button>
+                        <Button inverse onClick={openMapHandler}>
+                            VIEW ON MAP
+                        </Button>
+                        <Button to={`/places/${props.id}`}>
+                            EDIT
+                        </Button>
+                        <Button danger onClick={showDeleteWaringHandler}>
+                            DELETE
+                        </Button>
                     </div>
                 </Card>
 
--- client/src/places/pages/UpdatePlace.js
+++ client/src/places/pages/UpdatePlace.js
@@ -1,100 +1,135 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import { useForm } from "../../shared/hooks/form-hook";
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-import Input from "../../shared/components/FormElements/Input";
-import Button from "../../shared/components/FormElements/Button";
-import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/util/validators'
-
-import './PlaceForm.css'
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
+import Card from '../../shared/components/UIElements/Card';
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH
+} from '../../shared/util/validators';
+import { useForm } from '../../shared/hooks/form-hook';
+import './PlaceForm.css';
 
 const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire',
-        description: 'One of the famous',
-        imageUrl: 'https://blog.btcc.com/wp-content/uploads/2022/10/2022102705575983912_1666817880.png',
-        address: '무학공원',
-        location: {
-            lat: 35.824981,
-            lng: 128.639735
-        },
-        creator: 'u1'
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
     },
-    {
-        id: 'p2',
-        title: 'Empire',
-        description: 'One of the famous',
-        imageUrl: 'https://blog.btcc.com/wp-content/uploads/2022/10/2022102705575983912_1666817880.png',
-        address: '무학공원',
-        location: {
-            lat: 35.824981,
-            lng: 128.639735
-        },
-        creator: 'u2'
-    }
-]
+    creator: 'u1'
+  },
+  {
+    id: 'p2',
+    title: 'Emp. State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
+    },
+    creator: 'u2'
+  }
+];
+
 const UpdatePlace = () => {
-    const placeId = useParams().placeId;
-  
-    const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
-  
-    const [formState, inputHandler] = useForm(
-      {
-        title: {
-          value: identifiedPlace.title,
-          isValid: true
-        },
-        description: {
-          value: identifiedPlace.description,
-          isValid: true
-        }
+  const [isLoading, setIsLoading] = useState(true);
+  const placeId = useParams().placeId;
+
+  const [formState, inputHandler, setFormData] = useForm(
+    {
+      title: {
+        value: '',
+        isValid: false
       },
-      true
-    );
-  
-    const placeUpdateSubmitHandler = event => {
-      event.preventDefault();
-      console.log(formState.inputs);
-    };
-  
-    if (!identifiedPlace) {
-      return (
-        <div className="center">
-          <h2>Could not find place!</h2>
-        </div>
+      description: {
+        value: '',
+        isValid: false
+      }
+    },
+    false
+  );
+
+  const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
+
+  useEffect(() => {
+    if (identifiedPlace) {
+      setFormData(
+        {
+          title: {
+            value: identifiedPlace.title,
+            isValid: true
+          },
+          description: {
+            value: identifiedPlace.description,
+            isValid: true
+          }
+        },
+        true
       );
     }
-  
-    return (
-      <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
-        <Input
-          id="title"
-          element="input"
-          type="text"
-          label="Title"
-          validators={[VALIDATOR_REQUIRE()]}
-          errorText="Please enter a valid title."
-          onInput={inputHandler}
-          initialValue={formState.inputs.title.value}
-          initialValid={formState.inputs.title.isValid}
-        />
-        <Input
-          id="description"
-          element="textarea"
-          label="Description"
-          validators={[VALIDATOR_MINLENGTH(5)]}
-          errorText="Please enter a valid description (min. 5 characters)."
-          onInput={inputHandler}
-          initialValue={formState.inputs.description.value}
-          initialValid={formState.inputs.description.isValid}
-        />
-        <Button type="submit" disabled={!formState.isValid}>
-          UPDATE PLACE
-        </Button>
-      </form>
-    );
+    setIsLoading(false);
+  }, [setFormData, identifiedPlace]);
+
+  const placeUpdateSubmitHandler = event => {
+    event.preventDefault();
+    console.log(formState.inputs);
   };
-  
-  export default UpdatePlace;
-  
+
+  if (!identifiedPlace) {
+    return (
+      <div className="center">
+        <Card>
+          <h2>Could not find place!</h2>
+        </Card>
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <div className="center">
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
+
+  return (
+    <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+        onInput={inputHandler}
+        initialValue={formState.inputs.title.value}
+        initialValid={formState.inputs.title.isValid}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (min. 5 characters)."
+        onInput={inputHandler}
+        initialValue={formState.inputs.description.value}
+        initialValid={formState.inputs.description.isValid}
+      />
+      <Button type="submit" disabled={!formState.isValid}>
+        UPDATE PLACE
+      </Button>
+    </form>
+  );
+};
+
+export default UpdatePlace;
